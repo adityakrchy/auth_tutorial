8206f52f2d5693a62131cdedb8c1792f1db8727d
adityakrchy <adityakrchy101@gmail.com>
2022-01-21 23:10:47 +0530

    some changes made to notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -26,28 +26,46 @@ router.post("/createnewnote", fetchuser, async (req, res) => {
   }
 });
 // ROUTE 2: Update a existing note using: PUT "/notes/updatenote". Login required
-router.put('/updatenote/:id', fetchuser, async (req, res) => {
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
   const { title, description, tag } = req.body;
   try {
-      // Create a newNote object
-      const newNote = {};
-      if (title) { newNote.title = title };
-      if (description) { newNote.description = description };
-      if (tag) { newNote.tag = tag };
+    // Create a newNote object
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
 
-      // Find the note to be updated and update it
-      let note = await Notes.findById(req.params.id);
-      if (!note) { return res.status(404).send("Not Found") }
-
-      if (note.user.toString() !== req.user.id) {
-          return res.status(401).send("Not Allowed");
-      }
-      note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
-      res.json({ note });
+    // Find the note to be updated and update it
+    let note = await Notes.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+    note = await Notes.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+    res.json({ note });
   } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Internal Server Error");
+    console.log(error);
+    res.status(500).send("Internal Server Error");
   }
-})
+});
+
+// Route 4: Delete a note using: DELETE "/notes/deletenote/:id". Login required
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+  let note = await Notes.findById(req.params.id);
+  if (!note) {
+    return res.status(404).send("Not Found");
+  }
+  note = await Notes.findByIdAndDelete(req.params.id);
+  res.json({ message: "Note Deleted" });
+});
 
 module.exports = router;
